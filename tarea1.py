e503429b03ff7f3a5486f2d408cde6d5fe136652
eduardoh27 <ej.herreraa@uniandes.edu.co>
2022-09-12 01:39:39 -0500

    graph done

--- tarea1.py
+++ tarea1.py
@@ -2,23 +2,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 def main():
+
+    #plt.style.use(['science','no-latex'])
     
-    plt.style.use(['science','no-latex'])
+    def voltaje(t): 
+        print(t)
+        if t < 0:
+            return 18
+        else:
+            return 5.028*np.exp(-(50/9) * 1e6 * t) + 16.971
+
     
-    A = 1
-    
-    voltaje = lambda t : 5.028*np.exp(-(50/9) * 1e6 * t) + 16.971
-    
-    tiempo = np.linspace(0, 1e-6, 100)
-    
-    print(voltaje(1e-5))
-     
-    lista_y = voltaje(tiempo)
+    tiempo = np.linspace(-1e-6, 1e-6, 1000)
+    lista_y = [voltaje(i) for i in tiempo]
        
     fig = plt.figure()
     ax = fig.add_subplot()
-    ax.set_xlabel('Tiempo',fontsize=10)
-    ax.set_ylabel('$Voltaje$',fontsize=10)
+    ax.set_xlabel('$t$ (s)',fontsize=10)
+    ax.set_ylabel('$v(t)$ (V)',fontsize=10)
     plt.plot(tiempo, lista_y)
     
     plt.savefig('tarea1electronica.png', dpi=300, bbox_inches='tight')
